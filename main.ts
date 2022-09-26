09ae2e262a2c474d10e14523ad5db891747fb879
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2022-09-26 18:45:15 +0100

    Add button control

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
 radio.setGroup(0);
-loops.everyInterval(200, snore.recordVol);
-loops.everyInterval(200, snore.recordAccel);
-loops.everyInterval(10, snore.recordBP);
-loops.everyInterval(200, snore.sendData);
+let active = false;
+
+input.onButtonPressed(Button.A, function() {
+    active = !active;
+})
+
+loops.everyInterval(200, () => {
+    if (active) {
+        snore.recordVol();
+        snore.recordAccel();
+        snore.sendData();
+    }
+    });
+
+loops.everyInterval(200, () => {if (active){snore.sendData}});
