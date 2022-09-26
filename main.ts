3e4dc3ae0924eec3bec4aef1f7c31719aef0dd30
ruairidhMunro <101263395+ruairidhMunro@users.noreply.github.com>
2022-09-26 16:41:11 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-radio.setGroup(0)
-loops.everyInterval(100, snore.recordVol)
-loops.everyInterval(100, snore.recordAccel)
-loops.everyInterval(10, snore.recordBP)
-loops.everyInterval(100, snore.sendData)
+radio.setGroup(0);
+loops.everyInterval(100, snore.recordVol);
+loops.everyInterval(100, snore.recordAccel);
+loops.everyInterval(10, snore.recordBP);
+loops.everyInterval(100, snore.sendData);
