fca985cda7a91a3e1f0a3454270810c6d8b6f003
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2022-11-08 14:31:18 +0000

    Removed button controls

--- main.ts
+++ main.ts
@@ -1,42 +1,13 @@
 radio.setGroup(0);
-let active = false;
-let statusLed = true;
 let time = 0;
 
-const setLed = () => {
-    if (active && statusLed) {
-        led.plot(0,0);
-    } else {
-        led.unplot(0,0);
-    }
-}
-
-input.onButtonPressed(Button.A, () => {
-    active = !active;
-    setLed();
-    if (active) {
-        basic.showString("On");
-    } else {
-        basic.showString("Off");
-    }
-});
-
-input.onButtonPressed(Button.B, () => {
-    statusLed = !statusLed;
-    led.plot(0, 0);
-    basic.pause(1000);
-    setLed();
-});
-
 loops.everyInterval(snore.intervalSize / snore.bpMeasuresPerInterval, () => {
-    if (active) {
-        time += snore.intervalSize / snore.bpMeasuresPerInterval
-        snore.recordBP();
-        if (time == snore.intervalSize) {
-            snore.recordVol();
-            snore.recordAccel();
-            snore.sendData();
-            time = 0;
-        }
+    time += snore.intervalSize / snore.bpMeasuresPerInterval
+    snore.recordBP();
+    if (time == snore.intervalSize) {
+        snore.recordVol();
+        snore.recordAccel();
+        snore.sendData();
+        time = 0;
     }
 });
