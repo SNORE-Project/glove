e4369606f1862155858fa48ed77c5e6b12b494bf
Deranged-Dragon <102994713+Deranged-Dragon@users.noreply.github.com>
2023-02-23 13:06:26 +0000

    In-progress testing of dynamic pulse threshold

--- main.ts
+++ main.ts
@@ -4,6 +4,10 @@ let time2 = 0;
 let pulse_data = 0;
 let pulse_out = 0;
 let counter = 0;
+let stable_peak = 0;
+let running_peak = 0;
+let time_start = 0;
+let registered = 0;
 radio.setGroup(0);
 
 basic.forever(() => {
@@ -18,6 +22,18 @@ basic.forever(() => {
     serial.writeValue("Current Pulse", pulse_out);
 });
 
+basic.forever(() => {
+    if (pulse_data > running_peak){
+        running_peak = pulse_data;
+        time_start = input.runningTime();
+    } else if (input.runningTime() - time_start > 2 * delta_t && registered == 0) {
+        running_peak = 0
+    } else if (pulse_data < running_peak){
+        stable_peak = running_peak
+    } 
+    
+});
+
 function motion_magnitude() {
     return Math.abs(Math.sqrt(
         input.acceleration(Dimension.X) ** 2 +
@@ -26,8 +42,17 @@ function motion_magnitude() {
     ) - 1024);
 }
 
+/*
+    be constantly scanning for the peak of the current spike
+    if another spike is detected within -20% of that range then that's another pulse
+    if another spike isn't detected for 2*delta_t of previously recorded Pulse
+    reset what range we're looking for
+*/
+
 basic.forever(() => {
-    if (pulse_data > 800 && counter == 0) {
+    registered = 0;
+    if (pulse_data > (stable_peak - (stable_peak * 0.2)) && counter == 0) {
+        registered = 1;
         time2 = input.runningTime();
         delta_t = time2 - time1;
         time1 = time2;
