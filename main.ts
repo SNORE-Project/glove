4471bf19d82043331c15ccc1e4664eacaf36d340
Caleb Robson <94545082+Spartan2909@users.noreply.github.com>
2023-02-23 21:20:04 +0000

    Clean up code

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ let counter = 0;
 let stable_peak = 0;
 let running_peak = 0;
 let time_start = 0;
-let registered = 0;
+let registered = false;
 radio.setGroup(0);
 
 basic.forever(() => {
@@ -22,18 +22,6 @@ basic.forever(() => {
     serial.writeValue("Current Pulse", pulse_out);
 });
 
-basic.forever(() => {
-    if (pulse_data > running_peak){
-        running_peak = pulse_data;
-        time_start = input.runningTime();
-    } else if (input.runningTime() - time_start > 2 * delta_t && registered == 0) {
-        running_peak = 0
-    } else if (pulse_data < running_peak){
-        stable_peak = running_peak
-    } 
-    
-});
-
 function motion_magnitude() {
     return Math.abs(Math.sqrt(
         input.acceleration(Dimension.X) ** 2 +
@@ -43,16 +31,27 @@ function motion_magnitude() {
 }
 
 /*
-    be constantly scanning for the peak of the current spike
-    if another spike is detected within -20% of that range then that's another pulse
-    if another spike isn't detected for 2*delta_t of previously recorded Pulse
-    reset what range we're looking for
+    Constantly scan for the peak of the current spike. 
+    If another spike is detected within -20% of that range then that's another pulse. 
+    If another spike isn't detected for 2*delta_t of previously recorded pulse, 
+    reset what range we're looking for. 
 */
 
 basic.forever(() => {
-    registered = 0;
-    if (pulse_data > (stable_peak * 0.8) && counter == 0) {
-        registered = 1;
+    if (pulse_data > running_peak) {
+        running_peak = pulse_data;
+        time_start = input.runningTime();
+    } else if (input.runningTime() - time_start > 2 * delta_t && !registered) {
+        running_peak = 0
+    } else if (pulse_data < running_peak) {
+        stable_peak = running_peak
+    }
+});
+
+basic.forever(() => {
+    registered = false;
+    if (pulse_data > stable_peak * 0.8 && counter == 0) {
+        registered = true;
         time2 = input.runningTime();
         delta_t = time2 - time1;
         time1 = time2;
